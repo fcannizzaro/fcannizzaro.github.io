438f7799748e85ea2196b7a802dc733b562e8c8a
Francesco Saverio Cannizzaro <francescosaverio.cannizzaro@gmail.com>
2026-03-06 20:30:54 +0100

    remove drafts page from sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,27 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-import rehypeExternalLinks from 'rehype-external-links';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import rehypeExternalLinks from "rehype-external-links";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://fcannizzaro.com',
-	integrations: [mdx(), sitemap()],
-	vite: { plugins: [tailwindcss()] },
-	markdown: {
-		rehypePlugins: [
-			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
-		],
-	},
+  site: "https://fcannizzaro.com",
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => !page.includes("/drafts/"),
+    }),
+  ],
+  vite: { plugins: [tailwindcss()] },
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        { target: "_blank", rel: ["noopener", "noreferrer"] },
+      ],
+    ],
+  },
 });
